14f0611bf358c9ce71109021a0c1ae1dfa628597
Akromjon <akromjonrustamov56@gmail.com>
2024-05-10 22:55:21 +0500

    pushing...

---
+++ apps/photos/models.py
@@ -0,0 +1,30 @@
+from apps.shared.models import AbstractBaseModel
+from django.db import models
+
+
+
+class Tag(AbstractBaseModel):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = "tags"
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Photo(AbstractBaseModel):
+    title = models.CharField(max_length=80)
+    photo = models.ImageField(upload_to="photos/")
+    format = models.CharField(max_length=3)
+    dimension = models.CharField(max_length=9)
+    license = models.CharField(max_length=180)
+    tags = models.ManyToManyField(Tag, related_name="photos")
+    
+    class Meta:
+        verbose_name = 'photo'
+        verbose_name_plural = "photos"
+
+    def __str__(self) -> str:
+        return self.title
---
+++ apps/videos/models.py
@@ -0,0 +1,30 @@
+from apps.shared.models import AbstractBaseModel
+from django.db import models
+
+
+class Tag(AbstractBaseModel):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Video(AbstractBaseModel):
+    title = models.CharField(max_length=80)
+    resolution = models.CharField(max_length=9)
+    format = models.CharField(max_length=3)
+    duration = models.TimeField()
+    license = models.CharField(max_length=180)
+    tags = models.ManyToManyField(Tag, related_name="videos")
+
+    class Meta:
+        verbose_name = "video"
+        verbose_name_plural = "videos"
+
+    def __str__(self) -> str:
+        return self.title
+    
+
